6b5272b397f38d86e33ad997c855215d25d7896a
James Ellison <Jamesellison95@gmail.com>
2026-01-07 21:02:27 +0100

    profile
    Påbörjat

--- PortfolioProject/Controllers/ProfileController.cs
+++ PortfolioProject/Controllers/ProfileController.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PortfolioProject.Data;
+using PortfolioProject.Models;
 
 namespace PortfolioProject.Controllers
 {
     public class ProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<User> _userManager;
+        private readonly DatabaseContext _dbContext;
+        private readonly User _loggedIn;
+
+        public ProfileController(DatabaseContext dbContext, UserManager<User> userManager) {
+            _userManager = userManager;
+            _dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Index()
         {
+            var user = await _userManager.GetUserAsync(User);
+
+            return View(user);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit() {
+            var user = await _userManager.GetUserAsync(User);
+
             return View();
         }
+
+        //[HttpPost]
+        //public async Task<IActionResult> Edit() { 
+            //For saving later
+        //}
+
     }
 }
